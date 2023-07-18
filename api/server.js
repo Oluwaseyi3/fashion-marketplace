ee53b5780a51538fdf3fba8899ca7ac6d644dc25
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-07-18 14:40:52 +0100

    initialized db

--- api/server.js
+++ api/server.js
@@ -1,6 +1,17 @@
 import express from "express"
+import mongoose from "mongoose";
+import 'dotenv/config'
 
 const app = express()
+
+mongoose.set("strictQuery", true)
+
+try {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Connected to DB");
+} catch (error) {
+    console.log(error);
+}
 
 app.listen(8800, ()=> {
     console.log("Server is running");
