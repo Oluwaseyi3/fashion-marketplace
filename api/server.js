4ed5c8efb769e86356e4a24db81c63b3775fcfa5
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-07-19 14:30:21 +0100

    fixed routes bug

--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 import express from "express"
-import mongoose from "mongoose";
+import mongoose, { connect } from "mongoose";
 import 'dotenv/config'
+import userRoute from "./routes/user.route.js"
+import gigRoute from "./routes/gig.route.js"
+import orderRoute from "./routes/order.route.js"
+import conversationRoute from './routes/conversation.route.js'
+import messageRoute from "./routes/message.route.js"
+import reviewRoute from "./routes/review.route.js"
+import authRoute from "./routes/auth.route.js"
 
 const app = express()
 
@@ -12,6 +19,16 @@ try {
 } catch (error) {
     console.log(error);
 }
+
+app.use('/api/auth', authRoute)
+app.use("/api/user", userRoute)
+
+app.use('/api/users', gigRoute)
+app.use('/api/orders', orderRoute)
+app.use('/api/conversations', conversationRoute)
+app.use('/api/messages', messageRoute)
+app.use("/api/reviews", reviewRoute)
+
 
 app.listen(8800, ()=> {
     console.log("Server is running");
