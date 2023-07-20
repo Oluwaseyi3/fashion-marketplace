8031a68445bda1e6c9a7f91d1f1fef185ea2436e
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-07-20 10:15:46 +0100

    login route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,8 +1,15 @@
 import User from "../models/user.model.js"
+import bcrypt from "bcrypt"
+
 
 export const register = async(req, res) =>{
    try {
-     const newUser = new User(req.body)
+
+     const hash = bcrypt.hashSync(req.body.password, 5)
+     const newUser = new User({
+        password: hash,
+        ...req.body
+     })
 
       await newUser.save()
      console.log(newUser);
@@ -14,7 +21,20 @@ export const register = async(req, res) =>{
 }
 
 export const login = async(req, res) =>{
-    
+ 
+    try {
+        const user = await User.findOne({username: req.body.username})
+        if (!user) return res.status(404).send("User not found")
+            
+        const isCorrect = bcrypt.compareSync(req.body.password, user.password)
+        if (!isCorrect) return res.status(400).send("Wrong password or username")
+
+        const {password, ...info} = user
+        res.status(200).send(info)
+    } catch (error) {
+        res.status(500).send("Something went wrong")
+        console.log(error);
+    }
 }
 
 export const logout = async(req, res) =>{
