15c0b53b8709be3cf418b505db2b2ed843dcd860
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-07-20 15:05:21 +0100

    added middleware

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,13 +1,14 @@
 import User from "../models/user.model.js"
-import jwt from "jsonwebtoken"
+
 
 export const deleteUser = async(req, res) => {
 
-    const token = req.cookies.accessToken
-    if (!token) return res.status(401).send("You are not Authenticated")
+    const user = await User.findById(req.params.id)
+  
+        if (payload.id !== user._id.toString()) {
+          return res.status(403).send("You can only delete your account")
+        }
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).send("Deleted")
 
-    jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
-        res.send(payload)
-    })
-    // await User.findByIdAndDelete(req.params.id)
 }
