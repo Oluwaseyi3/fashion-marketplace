835aaaf491ef1d64c42c07ce8e6506b8c7f7d6e0
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-07-28 15:55:28 +0100

    feat: fixed Login component

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-       <div className='flex flex-col justify-between items-start'>
+    <div className='flex flex-col justify-center items-center w-full'>
+       <div className='flex flex-col justify-between items-start gap-3 min-w-[300px] p-5'>
        <div className='text-[40px]'>
            Sign In
         </div>
-        <div className=''>
-
+        <div className='text-[24px]'>
+           Username
+        </div>
+        <div className='w-full'>
+          <input className='w-full border-[1px] p-1'/>
+        </div>
+        <div className='text-[24px] ' >
+           Password
+        </div>
+        <div className='w-full'>
+          <input type="password" className='w-full border-[1px] p-1'/>
         </div>
        </div>
     </div>
