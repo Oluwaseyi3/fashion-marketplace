e6426647c3e0468a120b561289b7987fcaa3dde8
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-07-28 04:03:31 +0100

    feat: fixed gig ui

--- client/src/pages/gig/Gig.tsx
+++ client/src/pages/gig/Gig.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {AiOutlineStar} from "react-icons/ai"
+import Slider from 'infinite-react-carousel'
+import Buttons from '../../components/Buttons'
 
 const Gig = () => {
   return (
@@ -18,6 +20,54 @@ const Gig = () => {
             <div className='text-[24px] '>Anna Bell</div>
             <div className='flex gap-2 '>
                <AiOutlineStar/> <AiOutlineStar/> <AiOutlineStar/> <AiOutlineStar/> <AiOutlineStar/>
+            </div>
+          </div>
+
+          <div>
+          <Slider dots slidesToShow={1} autoplay={true} arrowsScroll={1} arrows={false} autoplayScroll pauseOnHover rows={1}>
+          <img src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1600" className='w-full h-[270px] '/>
+          <img src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1600" className='w-full h-[270px] '/>
+          <img src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1600" className='w-full h-[270px] '/>
+          <img src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1600" className='w-full h-[270px] '/>
+          <img src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1600" className='w-full h-[270px] '/>
+
+          </Slider>
+          </div>
+
+          <div className='text-[40px]'>
+            About This gig
+          </div>
+
+          <div className=''>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit sunt impedit, quam sint minima deserunt esse fugiat sapiente quasi explicabo. Eum, magnam necessitatibus! Similique laborum fugit temporibus quis optio quo.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, laudantium debitis officia accusantium dolorem numquam! Quaerat similique eos incidunt cumque qui doloremque rem id fugiat quidem blanditiis? Labore, sapiente beatae.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident suscipit ullam earum nemo natus aspernatur facilis dolorum animi error voluptatibus, minima soluta quae at necessitatibus eveniet delectus veritatis recusandae sed.
+            Lorem ipsum 
+          </div>
+
+          <div className='flex flex-col my-5 gap-3'>
+            <div className='text-[24px]'>
+              About the seller
+            </div>
+            <div className='flex gap-4 flex-wrap'>
+              <div>
+              <img src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1600" className='w-[70px] h-[70px] rounded-[100%]'/>
+              </div>
+
+              <div className='flex flex-col gap-2'>
+               
+                <div className='text-[24px]'>
+                  Anna Bell
+                </div>
+                <div className='flex gap-2 item-center'>
+                 <AiOutlineStar/> <AiOutlineStar/> <AiOutlineStar/> <AiOutlineStar/> <AiOutlineStar/> <div>5</div>
+                </div>
+                <div><Buttons title="Contact Me"/></div>
+              </div>
+            </div>
+
+            <div className='border w-full'>
+                 
             </div>
           </div>
         </div>
