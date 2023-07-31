0aebba7bc89e78723436326f5ec27ecefa5d5b81
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-07-31 14:35:11 +0100

    feat: fixed Login component ui

--- client/src/components/Buttons.tsx
+++ client/src/components/Buttons.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 interface Navprops{
     title: string;
-    onClick?: () => void
+    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    width? : string;
+    
 }
 
-const Buttons: React.FC<Navprops> = ({title, onClick}) => {
+const Buttons: React.FC<Navprops> = ({title, onClick, width}) => {
   return (
-    <button  type='button' onClick={onClick} className='py-2 px-6 rounded bg-purple-700 font-poppins font-medium text-[18px] text-white outline-none'>
+    <button  type='button' onClick={onClick} className={`py-2 px-6 rounded bg-purple-700 font-poppins font-medium text-[18px] text-white outline-none ${width}`}>
         {title}
     </button>
   )
--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,25 +1,41 @@
-import React from 'react'
+import { Button } from '@mui/material'
+import React, {useState} from 'react'
+import Buttons from '../../components/Buttons'
 
 const Login = () => {
+   
+const [username, setUsername] = useState("")
+const [password, setPassword] = useState("")
+const [error, setError] = useState()
+
+const handleSubmit = (e: any) => {
+  e.preventDefault()
+}
   return (
-    <div className='flex flex-col justify-center items-center w-full'>
-       <div className='flex flex-col justify-between items-start gap-3 min-w-[300px] p-5'>
-       <div className='text-[40px]'>
+
+    <div className='flex flex-col text-white bg-black justify-center items-center w-full'>
+      <form>
+       <div className='flex flex-col justify-between items-start gap-6 min-w-[300px] p-5'>
+       <div className='text-[40px] font-700'>
            Sign In
         </div>
         <div className='text-[24px]'>
            Username
         </div>
         <div className='w-full'>
-          <input className='w-full border-[1px] p-1'/>
+          <input className='w-full border-[1px] p-1' placeholder='maryjane' onChange={e=>setUsername(e.target.value)}/>
         </div>
         <div className='text-[24px] ' >
            Password
         </div>
         <div className='w-full'>
-          <input type="password" className='w-full border-[1px] p-1'/>
+          <input type="password" className='w-full border-[1px] p-1' placeholder='maryjane@gmail.com'
+          onChange={e=>setPassword(e.target.value)}
+          />
         </div>
+        <Buttons width="w-full" title='Login' onClick={handleSubmit}/>
        </div>
+       </form>
     </div>
   )
 }
