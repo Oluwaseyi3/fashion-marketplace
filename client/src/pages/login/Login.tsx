0c6f3257b10e56819e4c2990cb7633fd9ca1df78
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-07-31 15:42:05 +0100

    feat:updated login

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,20 +1,34 @@
 import { Button } from '@mui/material'
 import React, {useState} from 'react'
+import axios from 'axios'
 import Buttons from '../../components/Buttons'
 
 const Login = () => {
    
 const [username, setUsername] = useState("")
 const [password, setPassword] = useState("")
-const [error, setError] = useState()
+const [error, setError] = useState<any>(null)
 
-const handleSubmit = (e: any) => {
+const handleSubmit = async(e: any) => {
   e.preventDefault()
+
+  try {
+    const res = await axios.post("http://localhost:8800/api/auth/login", {
+      username, password
+    }, {withCredentials: true})
+    console.log(res.data);
+    
+  } catch (err) {
+    setError(err)
+  }
 }
   return (
 
-    <div className='flex flex-col text-white bg-black justify-center items-center w-full'>
+    <div className=' flex flex-col text-white bg-black justify-center items-center w-full'>
       <form>
+        <div>
+        
+        </div>
        <div className='flex flex-col justify-between items-start gap-6 min-w-[300px] p-5'>
        <div className='text-[40px] font-700'>
            Sign In
@@ -23,13 +37,15 @@ const handleSubmit = (e: any) => {
            Username
         </div>
         <div className='w-full'>
-          <input className='w-full border-[1px] p-1' placeholder='maryjane' onChange={e=>setUsername(e.target.value)}/>
+          <input className='w-full text-black p-1 font-light bg-white border-2 rounded-md peer 
+                 outline-none transition disabled:opacity-70  ' placeholder='maryjane' onChange={e=>setUsername(e.target.value)} />
         </div>
-        <div className='text-[24px] ' >
+        <div className='text-[24px]' >
            Password
         </div>
         <div className='w-full'>
-          <input type="password" className='w-full border-[1px] p-1' placeholder='maryjane@gmail.com'
+          <input type="password" className=' w-full text-black p-1 font-light bg-white border-2 rounded-md peer 
+                 outline-none transition disabled:opacity-70  ' placeholder='maryjane@gmail.com'
           onChange={e=>setPassword(e.target.value)}
           />
         </div>
